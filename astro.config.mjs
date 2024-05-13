8be90fe64e16d96ba1053c6570a14ea7f9b3e98f
Kenneth <kennethnym@outlook.com>
2024-05-14 00:01:06 +0100

    use catppuccin theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kennethnym.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://kennethnym.com",
+	integrations: [mdx(), sitemap(), tailwind()],
+	markdown: {
+		shikiConfig: {
+			// Choose from Shiki's built-in themes (or add your own)
+			// https://shiki.style/themes
+			theme: "catppuccin-mocha",
+		},
+	},
 });
