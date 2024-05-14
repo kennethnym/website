c95923d30ff7f8fd8cbb7298d798ea5be5624a83
Kenneth <kennethnym@outlook.com>
2024-05-15 00:51:50 +0100

    WIP: vim emulation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ import catppuccin from "@catppuccin/tailwindcss";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	safelist: ["bg-peach", "text-red"],
 	theme: {
 		extend: {},
 	},
